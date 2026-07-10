975b6506610e33d29cc1970863c1cb473f5c4afc
nathanielcraigbrown <nathanielcraigbrown@gmail.com>
2026-07-10 15:21:41 -0700

    Load consensus dashboard styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './workspace.css'
+import './app.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
